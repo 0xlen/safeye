13d6779bcea75a6100b2fef60db24c29860c1e70
Peter Jian <arashi30801@gmail.com>
2016-12-26 18:42:28 +0800

    mail and file
    can read json for print table
    can read file url for virustotal but virustotal api need to update

--- background_scripts/main.js
+++ background_scripts/main.js
@@ -3,3 +3,16 @@ chrome.runtime.onConnect.addListener(function(port) {
 
 chrome.browserAction.onClicked.addListener(function(tab) {
 });
+
+chrome.downloads.onCreated.addListener(function(callback){
+    chrome.downloads.pause(callback.id);
+    var api = 'http://safeye.azurewebsites.net/api/url_scan.php';
+    var params = {
+        'url': callback.url
+    };
+    $.post(api, params, function(data) {
+    	console.log(data);
+    	chrome.downloads.resume(callback.id);
+        alert(data.positives + '/' + data.total);
+    }, 'json');
+});
