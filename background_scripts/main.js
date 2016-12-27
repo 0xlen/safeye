527353755da51d5242ca4cc3f719493ed8b9baef
Peter Jian <arashi30801@gmail.com>
2016-12-28 04:20:53 +0800

    marge mail and file and page url
    three method is done

--- background_scripts/main.js
+++ background_scripts/main.js
@@ -1,22 +1,26 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
+chrome.runtime.onConnect.addListener(function(port) {
 });
 
-chrome.runtime.onConnect.addListener(function(port) {
-
+chrome.browserAction.onClicked.addListener(function(tab) {
 });
 
 
 chrome.downloads.onCreated.addListener(function(callback){
-    var api = 'http://10.104.4.171/safeye/api/url_scan.php';
+    var api = 'http://192.168.1.104/safeye/api/url_scan.php';
     var params = {
         'url': callback.url ,
         'options': {
             'scan' : 1
         }
     };
+    
     $.post(api, params, function(data) {
         console.log(data);
         if(data.positives > 0){
+            alert("警告 " + callback.url + "此下載連結 \n在 " +
+                data.total + " 處中有 " + data.positives + " 處偵測為惡意");
+        }
+        else if(data.positives > 0){
             alert("警告 " + callback.url + "此下載連結 \n在 " +
                 data.total + " 處中有 " + data.positives + " 處偵測為惡意");
         }
