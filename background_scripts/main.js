1cf7b1ed0c112718652174474e7f4dca30a087a4
Peter Jian <arashi30801@gmail.com>
2016-12-27 22:37:29 +0800

    can read file and mail
    like title say
    remenber lib/main CLIENT_ID TO YOUR GOOGLE API KEY

--- background_scripts/main.js
+++ background_scripts/main.js
@@ -1,18 +1,24 @@
-chrome.runtime.onConnect.addListener(function(port) {
-});
-
 chrome.browserAction.onClicked.addListener(function(tab) {
 });
 
+chrome.runtime.onConnect.addListener(function(port) {
+
+});
+
+
 chrome.downloads.onCreated.addListener(function(callback){
-    chrome.downloads.pause(callback.id);
-    var api = 'http://safeye.azurewebsites.net/api/url_scan.php';
+    var api = 'http://10.104.4.171/safeye/api/url_scan.php';
     var params = {
-        'url': callback.url
+        'url': callback.url ,
+        'options': {
+            'scan' : 1
+        }
     };
     $.post(api, params, function(data) {
-    	console.log(data);
-    	chrome.downloads.resume(callback.id);
-        alert(data.positives + '/' + data.total);
+        console.log(data);
+        if(data.positives > 0){
+            alert("警告 " + callback.url + "此下載連結 \n在 " +
+                data.total + " 處中有 " + data.positives + " 處偵測為惡意");
+        }
     }, 'json');
 });
